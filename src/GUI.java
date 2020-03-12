25a910781abf74d97d050941f51dafdd3ac494cf
alexander.holmstro3 <alle020916@gmail.com>
2020-03-12 11:00:56 +0100

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GUI {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JButton Save;
+    private JButton Openfile;
+    private JButton button1;
+
+}
